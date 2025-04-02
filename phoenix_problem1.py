e09e7552127461cb5a3ecc9ec8c0c63f8b545e0f
Sanjay.M <137694520+Sanjay-Mx21@users.noreply.github.com>
2025-04-02 13:48:18 +0530

    Add files via upload

--- phoenix_problem1.py
+++ phoenix_problem1.py
@@ -1,3 +1,17 @@
 # sanjay
 a=int(input())
-a=[["1","1","1","1","0"],["1","1","0","1","1"],["1","1","0","0","0"],["",]]
+a=[["1","1","1","1","0"],["1","1","0","1","1"],["1","1","0","0","0"]]
+
+def numIslands(grid):
+    if not grid:
+        return 0
+
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == "1":
+                numIslands(grid, i, j)
+                count += 1
+    return count
+
+print(numIslands(a))
