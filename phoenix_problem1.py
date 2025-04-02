e736fad53b3b402fd47b92ad257c9f7f7029432b
Sanjay.M <137694520+Sanjay-Mx21@users.noreply.github.com>
2025-04-02 14:19:06 +0530

    Add files via upload

--- phoenix_problem1.py
+++ phoenix_problem1.py
@@ -1,17 +1,49 @@
 # sanjay
-a=int(input())
 a=[["1","1","1","1","0"],["1","1","0","1","1"],["1","1","0","0","0"]]
+b = [['1','1','1','0','0'],['1','1','0','0','0'],['0','0','0','1','1'],['0','0','1','1','1']]
 
-def numIslands(grid):
-    if not grid:
-        return 0
+def helper(grid,i,j):
+    try:
+        if grid[i][j]=='1':
+            grid[i][j]='0'
+        if grid[i+1][j]=='1':
+            helper(grid,i+1,j)
+        
+        if grid[i][j+1]=='1':
+            helper(grid,i,j+1)
 
-    count = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == "1":
-                numIslands(grid, i, j)
-                count += 1
+
+        if grid[i-1][j]=='1':
+            helper(grid,i+1,j)
+        if grid[i][j-1]=='1':
+            helper(grid,i,j+1)
+    except IndexError:
+        pass
+
+def numIslands(grid,i,j,count):
+    
+    try:
+        for i in range(len(grid)): 
+            for j in range(len(grid[0])):
+                if grid[i][j]=='1':
+                    count+=1
+                    grid[i][j]='0'
+                    for line in grid:
+                        print(line)
+                    print()
+                    if grid[i+1][j]=='1':
+                        helper(grid,i+1,j)
+                    if grid[i][j+1]=='1':
+                        helper(grid,i,j+1)
+                    if grid[i-1][j]=='1':
+                        helper(grid,i+1,j)
+                    if grid[i][j-1]=='1':
+                        helper(grid,i,j+1)
+                                
+                
+    except IndexError:
+        pass
     return count
-
-print(numIslands(a))
+for i in b:
+    print(i)
+print(numIslands(b,0,0,0))
